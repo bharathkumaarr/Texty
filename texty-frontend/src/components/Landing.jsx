5a63584282d0758134252edc447cd6e9d906c62c
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-20 19:54:23 +0530

    update

--- texty-frontend/src/components/Landing.jsx
+++ texty-frontend/src/components/Landing.jsx
@@ -10,10 +10,10 @@ function Landing({onCreateRoom, onJoinRoom}) {
         <h1 className='sm:text-3xl text-xl mb-2 text-center'>Create instant chat rooms with Texty.</h1>
         <h1 className='text-lg sm:text-xl text-center'>Generate a unique code, share it, and chat!</h1>
         <h1 className='text-lg sm:text-xl mb-4 text-center'>Rooms vanish when everyone leaves, no sign-up needed.</h1>
-        <h6 className='text-sm sm:text-lg text-center'>------------------------------------------------------------</h6>
+        <h6 className='text-sm sm:text-lg text-center'>---------------------------------------------</h6>
         <button
             onClick={onCreateRoom}
-            className='bg-gray-200 hover:bg-gray-300 h-11 w-full max-w-xs rounded-xl text-lg font-semibold mb-2 border border-neutral-900'
+            className='bg-gray-200 hover:bg-gray-300 h-11 w-full max-w-xs rounded-xl text-lg font-semibold mb-2 border border-neutral-900 mt-4'
         >
             Create New Room
         </button>
