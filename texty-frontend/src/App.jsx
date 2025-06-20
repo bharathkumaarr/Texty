585a1827e0a54eee3e48c08ecb58791391450f8a
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-20 17:03:56 +0530

    update

--- texty-frontend/src/App.jsx
+++ texty-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react'
 
 import './App.css'
 import Landing from './components/Landing'
+import ChatRoom from './components/ChatRoom'
 import {io} from 'socket.io-client'
 
 const socket = io('http://localhost:3000')
@@ -18,19 +19,19 @@ function App() {
     socket.emit('create-room');
   };
 
-  const handleJoinRoom =() =>{
+  const handleJoinRoom =(code) =>{
     socket.emit('join-room', code);
     setRoomCode(code)
   }
 
-  if (!roomCode) {
-    return <Landing onCreateRoom={handleCreateRoom} onJoinRoom={handleJoinRoom} />
-  }
-
   return (
-    <div className='min-h-screen bg-slate-200 text-neutral-900 flex items-center justify-center'>
-      <h2 className='text-3xl'> Joined Room: {roomCode}</h2>
-    </div>
+    <>
+      {!roomCode ? (
+        <Landing onCreateRoom={handleCreateRoom} onJoinRoom={handleJoinRoom} />
+      ) : (
+        <ChatRoom socket={socket} roomCode={roomCode} />
+      )}
+    </>
   ) 
 }
 
